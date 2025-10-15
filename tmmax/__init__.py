c5b81ed68982359f829ed1cbfcf521bbf7900dd4
bahremsd <bdanis23@ku.edu.tr>
2025-10-15 18:56:32 +0300

    finalize revision and update version

--- tmmax/__init__.py
+++ tmmax/__init__.py
@@ -1,6 +1,6 @@
 """TMMax: High-performance modeling of multilayer thin-film structures using transfer matrix method with JAX"""
 
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 
 description = "tmmax is a high-performance computational library designed for efficient calculation of optical properties in multilayer thin-film structures. Engineered to serve as a Swiss Army knife tool for thin-film optics research, tmmax integrates a comprehensive suite of advanced numerical methods. At its core, tmmax leverages JAX to enable just-in-time (JIT) compilation, vectorization and XLA (Accelerated Linear Algebra) operations, dramatically accelerating the evaluation of optical responses in multilayer coatings. By exploiting these capabilities, tmmax achieves exceptional computational speed, making it the optimal choice for modeling and analyzing complex systems."
 
